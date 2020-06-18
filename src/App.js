ef1cb1544a1844a820c188db9937dd17ea6f0b41
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-06-18 18:02:10 +0530

    Initial structure added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Homepage from './Homepage.component'
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div className='App'>
+      <Homepage />
+    </div>;
   }
 }
 
---
+++ src/Homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './homepage.styles.scss'
+export default function Homepage() {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">title</h1>
+                        <span className="subtitle">shop now!</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">title</h1>
+                        <span className="subtitle">shop now!</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">title</h1>
+                        <span className="subtitle">shop now!</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">title</h1>
+                        <span className="subtitle">shop now!</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">title</h1>
+                        <span className="subtitle">shop now!</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
