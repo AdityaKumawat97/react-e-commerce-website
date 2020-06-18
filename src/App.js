1e78f2b24dc0eaf9eb1e1d386dee0b42dbc0cd52
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-06-18 16:04:17 +0530

    Initial commit
    Bootstrapping react app using create-react-app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className='App'>Hello World</div>;
+  }
+}
+
+export default App;
