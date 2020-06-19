19f5a670fec0ee5960ecc940c8f2d389c452ed58
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-06-19 22:08:28 +0530

    rendering dynamic images added

--- src/components/directory/Directory.component.jsx
+++ src/components/directory/Directory.component.jsx
@@ -44,8 +44,8 @@ export default class Directory extends Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({ title, imageUrl, id }) => (
-                        <MenuItem key={id} title={title} />
+                    this.state.sections.map(({ title, imageUrl, id, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                     ))
                 }
             </div>
--- src/components/menu-item/Menu-item.component.jsx
+++ src/components/menu-item/Menu-item.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './menu-item.styles.scss'
-export default function MenuItem({ title }) {
+export default function MenuItem({ title, imageUrl, size }) {
     return (
-        <div className="menu-item">
+        <div style={{ backgroundImage: `url(${imageUrl})` }} className={`${size} menu-item`}>
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">shop now!</span>
